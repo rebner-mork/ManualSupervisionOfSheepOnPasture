2a8091d85f9d349a26cf08ceffaa498c1387103b
morkolai <nikolai-mork@live.no>
2022-01-19 09:52:23 +0100

    Using flex to structure login form widget and added button for createing user account

--- web/lib/main.dart
+++ web/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+//TODO separate fpage in more files
+
 void main() {
   runApp(const MyApp());
 }
@@ -86,87 +88,64 @@ class _LoginFormState extends State<LoginForm> {
     return Form(
         key: _formKey,
         child: Column(children: [
-          const Flexible(
-              child: FractionallySizedBox(
-            heightFactor: 0.7,
-          )),
+          const Spacer(flex: 10),
           const Icon(
             Icons.account_circle,
             size: 200,
           ),
-          const Flexible(
-              child: FractionallySizedBox(
-            heightFactor: 0.1,
-          )),
-          TextFormField(
-            autofocus: true,
-            textAlign: TextAlign.left,
-            validator: _validateUserName,
-            decoration: const InputDecoration(
-                hintText: "Brukernavn", border: OutlineInputBorder()),
-          ),
-          const Flexible(
-              child: FractionallySizedBox(
-            heightFactor: 0.1,
-          )),
-          TextFormField(
-              textAlign: TextAlign.left,
-              validator: _validatePassword,
-              obscureText: _visiblePassword,
-              decoration: InputDecoration(
-                  hintText: "Passord",
-                  suffixIcon: IconButton(
-                      icon: const Icon(Icons.visibility),
-                      onPressed: _toggleVisiblePassword),
-                  border: const OutlineInputBorder())),
-          const Flexible(
-              child: FractionallySizedBox(
-            heightFactor: 0.1,
-          )),
-          ElevatedButton(
-              onPressed: () {
-                _formKey.currentState!.validate();
-              },
-              child: const Text("Logg inn"))
-        ]));
-  }
-}
-
-    /*return Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            const Icon(Icons.account_circle, size: 180),
-            const SizedBox(
-              height: 40,
-            ),
-            TextFormField(
-              autofocus: true,
-              textAlign: TextAlign.left,
-              validator: _validateUserName,
-              decoration: const InputDecoration(
-                  hintText: "Brukernavn", border: OutlineInputBorder()),
-            ),
-            const SizedBox(height: 30 //TODO sette til const,
-                ),
-            TextFormField(
+          const Spacer(flex: 2),
+          Flexible(
+              flex: 10,
+              child: TextFormField(
+                autofocus: true,
+                textAlign: TextAlign.left,
+                validator: _validateUserName,
+                decoration: const InputDecoration(
+                    hintText: "Brukernavn", border: OutlineInputBorder()),
+              )),
+          const Spacer(),
+          Flexible(
+            flex: 10,
+            child: TextFormField(
                 textAlign: TextAlign.left,
                 validator: _validatePassword,
                 obscureText: _visiblePassword,
                 decoration: InputDecoration(
                     hintText: "Passord",
                     suffixIcon: IconButton(
-                        icon: const Icon(Icons.visibility),
+                        icon: Icon(_visiblePassword
+                            ? Icons.visibility_off
+                            : Icons.visibility),
                         onPressed: _toggleVisiblePassword),
                     border: const OutlineInputBorder())),
-            const SizedBox(height: 40 //TODO sette til const,
-                ),
-            ElevatedButton(
+          ),
+          const Spacer(flex: 2),
+          Flexible(
+              flex: 10,
+              child: ElevatedButton(
                 onPressed: () {
                   _formKey.currentState!.validate();
                 },
-                child: const Text("Logg inn"))
-          ],
-        ));
-  }*/
-
+                child: const Text("Logg inn"),
+                style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(300, 60),
+                    textStyle: const TextStyle(fontSize: 30)),
+              )),
+          const Spacer(flex: 10),
+          const Flexible(
+              flex: 4,
+              child: Text(
+                  "Om du ikker har en brukerkonto ennå, kan du oprette en her:")),
+          const Spacer(),
+          Flexible(
+              flex: 10,
+              child: ElevatedButton(
+                onPressed: () {
+                  print("Hello world"); // TODO add functionality
+                },
+                child: const Text("Opprett brukerkonto"),
+                style: ElevatedButton.styleFrom(fixedSize: const Size(200, 50)),
+              ))
+        ]));
+  }
+}
