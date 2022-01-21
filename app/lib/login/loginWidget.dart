cc057e0625499f381294734316214168067c285f
Anders H. Rebner <anderhre@stud.ntnu.no>
2022-01-21 12:42:23 +0100

    Unit tests, moved authentication logic to utils

--- app/lib/login/loginWidget.dart
+++ app/lib/login/loginWidget.dart
@@ -113,25 +113,6 @@ class _LoginWidgetState extends State<LoginWidget> {
         ));
   }
 
-  String? validateEmail(String? email) {
-    if (email!.isEmpty) {
-      return 'Skriv e-post';
-    } else if (!EmailValidator.validate(email)) {
-      return 'Skriv gyldig e-post';
-    }
-    return null;
-  }
-
-  String? validatePassword(String? password) {
-    if (password!.isEmpty) {
-      return 'Skriv passord';
-    } else if (password.length < 8) {
-      return 'Passord må inneholde minst 8 tegn';
-    }
-
-    return null;
-  }
-
   Future<void> signIn() async {
     final formState = _formKey.currentState;
     if (formState!.validate()) {
--- app/lib/utils/authentication.dart
+++ app/lib/utils/authentication.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future<UserCredential> signInWithEmailAndPassword(
@@ -6,4 +7,23 @@ Future<UserCredential> signInWithEmailAndPassword(
       .signInWithEmailAndPassword(email: email, password: password);
 
   return user;
+}
+
+String? validateEmail(String? email) {
+  if (email!.isEmpty) {
+    return 'Skriv e-post';
+  } else if (!EmailValidator.validate(email)) {
+    return 'Skriv gyldig e-post';
+  }
+  return null;
+}
+
+String? validatePassword(String? password) {
+  if (password!.isEmpty) {
+    return 'Skriv passord';
+  } else if (password.length < 8) {
+    return 'Passord må inneholde minst 8 tegn';
+  }
+
+  return null;
 }
