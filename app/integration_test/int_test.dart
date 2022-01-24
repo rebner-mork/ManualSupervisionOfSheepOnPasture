8294ac654ecd412e9244053114d9ce975749c11c
Anders H. Rebner <anderhre@stud.ntnu.no>
2022-01-24 10:53:38 +0100

    Integration test

--- app/integration_test/int_test.dart
+++ app/integration_test/int_test.dart
@@ -6,8 +6,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'dart:developer' as logger;
 
+// This test can only run on an emulator (not on a pysical device)
 void main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -17,6 +17,29 @@ void main() async {
     String host = Platform.isAndroid ? '10.0.2.2' : 'localhost';
     FirebaseAuth.instance.useAuthEmulator(host, 9099);
 
-    // TODO: test
+    var emailField = find.byKey(const Key('inputEmail'));
+    var passwordField = find.byKey(const Key('inputPassword'));
+    var loginButton = find.text('Logg inn');
+
+    // Assert user is not logged in
+    expect(FirebaseAuth.instance.currentUser, null);
+
+    // Create user
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: 'test@gmail.com', password: '12345678');
+    } catch (e) {
+      fail(e.toString());
+    }
+
+    // Enter login information and log in
+    await tester.enterText(emailField, 'test@gmail.com');
+    await tester.enterText(passwordField, '12345678');
+    await tester.tap(loginButton);
+    await tester.pump();
+
+    // Assert user is logged in
+    expect(FirebaseAuth.instance.currentUser, isNotNull);
+    expect(FirebaseAuth.instance.currentUser, const TypeMatcher<User>());
   });
 }
