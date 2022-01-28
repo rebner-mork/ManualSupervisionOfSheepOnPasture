2922976951091238c4ef0126e0771404184b83e1
Anders H. Rebner <anderhre@stud.ntnu.no>
2022-01-28 12:56:45 +0100

    Separated workflows, added cache in app workflow

--- app/integration_test/int_test.dart
+++ app/integration_test/int_test.dart
@@ -1,10 +1,8 @@
 import 'dart:io';
 
 import 'package:app/login/login_page.dart';
-import 'package:app/main.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
