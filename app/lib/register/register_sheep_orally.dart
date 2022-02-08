7d80dea22286b9c9681b882f7535e8e0052b21e4
Anders H. Rebner <anderhre@stud.ntnu.no>
2022-02-08 20:33:45 +0100

    Added inputfields and questions, dialogue automatocally starts

--- app/lib/register/register_sheep_orally.dart
+++ app/lib/register/register_sheep_orally.dart
@@ -1,9 +1,16 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:app/utils/custom_widgets.dart';
 import 'package:app/utils/question_sets.dart';
 import 'package:app/utils/speech_input_filters.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
+import 'package:fluttericon/rpg_awesome_icons.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
 
 class RegisterSheepOrallyWidget extends StatefulWidget {
   const RegisterSheepOrallyWidget(this.fileName, {Key? key}) : super(key: key);
@@ -15,7 +22,8 @@ class RegisterSheepOrallyWidget extends StatefulWidget {
   State<RegisterSheepOrallyWidget> createState() => _RegisterSheepOrallyState();
 }
 
-// TODO: Bare én instanse av STT per applikasjon, dvs. at den må passes inn i denne widgeten(?)
+// TODO: Bare én instanse av STT per applikasjon, dvs. at den
+// muligens må passes inn i denne widgeten(?)
 // https://pub.dev/packages/speech_to_text#initialize-once
 
 enum TtsState { speaking, notSpeaking }
@@ -45,11 +53,33 @@ class _RegisterSheepOrallyState extends State<RegisterSheepOrallyWidget> {
 
   bool _speechEnabled = false;
 
+  final scrollController = ScrollController();
+  final headlineTwoKey = GlobalKey();
+  final headlineThreeKey = GlobalKey();
+
+  final _formKey = GlobalKey<FormState>();
+
+  final _textControllers = <String, TextEditingController>{
+    'sheep': TextEditingController(),
+    'lambs': TextEditingController(),
+    'black': TextEditingController(),
+    'white': TextEditingController(),
+    'blackHead': TextEditingController(),
+    'redTie': TextEditingController(),
+    'blueTie': TextEditingController(),
+    'yellowTie': TextEditingController(),
+    'redEar': TextEditingController(),
+    'blueEar': TextEditingController(),
+  };
+
   @override
   void initState() {
     super.initState();
     _initTextToSpeech();
     _initSpeechToText();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _startDialog(allSheepQuestions, allSheepQuestionsContexts);
+    });
   }
 
   void _initSpeechToText() async {
@@ -73,21 +103,14 @@ class _RegisterSheepOrallyState extends State<RegisterSheepOrallyWidget> {
   Future _listen(QuestionContext questionContext) async {
     // Mange options, se example
     await _stt.listen(
-      partialResults: false,
-      onResult: (result) => _onSpeechResult(result, questionContext),
-    ); // listenFor is max - NOT min
-    //localeId: 'en_US',
-    /*  onDevice: true,
-        listenFor: const Duration(seconds: 5)*/
-    /*if (index != maxIndex) {
-      String response = "Jeg forstod ikke.";
-      await _speak(response);
-      await _speak(allSheepQuestions[index]);
-      await _listen(allSheepQuestionsContexts[index]);
-    }*/
+        partialResults: false,
+        onResult: (result) => _onSpeechResult(result, questionContext),
+        localeId: 'en_US',
+        onDevice: true,
+        listenFor: const Duration(seconds: 5) // listenFor is max - NOT min
+        );
   }
 
-  // TODO: support "previous"
   void _onSpeechResult(
       SpeechRecognitionResult result, QuestionContext questionContext) async {
     // Always true when 'partialResults: false'
@@ -127,9 +150,9 @@ class _RegisterSheepOrallyState extends State<RegisterSheepOrallyWidget> {
           await _speak(spokenWord, language: 'en-US');
 
           setState(() {
-            nonFilteredAnswers.insert(
-                index, result.recognizedWords); //add(result.recognizedWords);
-            filteredAnswers.insert(index, spokenWord);
+            _textControllers[index]!.text = spokenWord;
+            //nonFilteredAnswers.insert(index, result.recognizedWords);
+            //filteredAnswers.insert(index, spokenWord);
           });
 
           index++;
@@ -215,24 +238,177 @@ class _RegisterSheepOrallyState extends State<RegisterSheepOrallyWidget> {
     debugPrint('Stemmer: ${voices.toString()}');
   }
 
+  _registerSheep() async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final String path = directory.path;
+
+    final File file = File('$path/${widget.fileName}.json');
+    final Map data = _gatherData();
+
+    file.writeAsString(json.encode(data));
+  }
+
+  Map _gatherData() {
+    return <String, int>{
+      'sheep': _textControllers['sheep']!.text.isEmpty
+          ? 0
+          : int.parse(_textControllers['sheep']!.text),
+      'lambs': _textControllers['lambs']!.text.isEmpty
+          ? 0
+          : int.parse(_textControllers['lambs']!.text),
+      'white': _textControllers['white']!.text.isEmpty
+          ? 0
+          : int.parse(_textControllers['white']!.text),
+      'black': _textControllers['black']!.text.isEmpty
+          ? 0
+          : int.parse(_textControllers['black']!.text),
+      'blackHead': _textControllers['blackHead']!.text.isEmpty
+          ? 0
+          : int.parse(_textControllers['blackHead']!.text),
+      'redTie': _textControllers['redTie']!.text.isEmpty
+          ? 0
+          : int.parse(_textControllers['redTie']!.text),
+      'blueTie': _textControllers['blueTie']!.text.isEmpty
+          ? 0
+          : int.parse(_textControllers['blueTie']!.text),
+      'yellowTie': _textControllers['yellowTie']!.text.isEmpty
+          ? 0
+          : int.parse(_textControllers['yellowTie']!.text),
+      'redEar': _textControllers['redEar']!.text.isEmpty
+          ? 0
+          : int.parse(_textControllers['redEar']!.text),
+      'blueEar': _textControllers['blueEar']!.text.isEmpty
+          ? 0
+          : int.parse(_textControllers['blueEar']!.text),
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Scaffold(
-            appBar: AppBar(
-              title: const Text('Registrer sau muntlig'),
-            ),
-            body: Column(
-              children: [
-                ElevatedButton(
-                    onPressed: () => _startDialog(
-                        allSheepQuestions, allSheepQuestionsContexts),
-                    child: const Text('Spytt ut')),
-                Text("Ikke-filtrert: ${nonFilteredAnswers.toString()}"),
-                Text("Filtrert:      ${filteredAnswers.toString()}"),
-                /*ElevatedButton(
+        child: Form(
+            key: _formKey,
+            child: Scaffold(
+                appBar: AppBar(
+                  title: const Text('Registrer sau muntlig'),
+                  leading: BackButton(
+                      onPressed: () => {
+                            if (isListening)
+                              {
+                                _stt.stop() // TODO: sjekk om fungerer
+                              },
+                            showDialog(
+                                context: context,
+                                builder: (_) =>
+                                    cancelRegistrationDialog(context))
+                          }),
+                ),
+                body: SingleChildScrollView(
+                    controller: scrollController,
+                    child: Center(
+                        child: Column(children: [
+                      const SizedBox(height: 10),
+                      inputDividerWithHeadline('Antall'),
+                      inputRow('Sauer', _textControllers['sheep']!,
+                          RpgAwesome.sheep, Colors.grey),
+                      inputFieldSpacer(),
+                      inputRow('Lam', _textControllers['lambs']!,
+                          RpgAwesome.sheep, Colors.grey,
+                          iconSize: 24),
+                      inputFieldSpacer(),
+
+                      inputRow(
+                        'Hvite',
+                        _textControllers['white']!,
+                        RpgAwesome.sheep,
+                        Colors.white,
+                      ),
+                      inputFieldSpacer(),
+                      inputRow(
+                        'Svarte',
+                        _textControllers['black']!,
+                        RpgAwesome.sheep,
+                        Colors.black,
+                      ),
+                      inputFieldSpacer(),
+                      inputRow('Svart hode', _textControllers['blackHead']!,
+                          RpgAwesome.sheep, Colors.black,
+                          scrollController: scrollController,
+                          fieldAmount: 5,
+                          key: headlineTwoKey),
+
+                      inputDividerWithHeadline('Slips', headlineTwoKey),
+
+                      // TODO: Conditional basert på mulige farger
+                      inputRow(
+                        'Røde',
+                        _textControllers['redTie']!,
+                        FontAwesome5.black_tie,
+                        Colors.red,
+                      ),
+                      inputFieldSpacer(),
+                      inputRow(
+                        'Blå',
+                        _textControllers['blueTie']!,
+                        FontAwesome5.black_tie,
+                        Colors.blue,
+                      ),
+                      inputFieldSpacer(),
+                      inputRow('Gule', _textControllers['yellowTie']!,
+                          FontAwesome5.black_tie, Colors.yellow,
+                          scrollController: scrollController,
+                          fieldAmount: 3,
+                          key: headlineThreeKey),
+                      // TODO: Conditional basert på mulige farger
+
+                      inputDividerWithHeadline('Øremerker', headlineThreeKey),
+
+                      inputRow(
+                        'Røde',
+                        _textControllers['redEar']!,
+                        Icons.local_offer,
+                        Colors.red,
+                      ),
+                      inputFieldSpacer(),
+                      inputRow(
+                        'Blå',
+                        _textControllers['blueEar']!,
+                        Icons.local_offer,
+                        Colors.blue,
+                      ),
+                      inputFieldSpacer(),
+                      const SizedBox(height: 80),
+                    ]))),
+                floatingActionButton:
+                    MediaQuery.of(context).viewInsets.bottom == 0
+                        ? FloatingActionButton.extended(
+                            onPressed: _registerSheep,
+                            label: const Text('Fullfør registrering',
+                                style: TextStyle(fontSize: 19)))
+                        : FloatingActionButton(
+                            onPressed: _registerSheep,
+                            child: const Icon(
+                              Icons.check,
+                              size: 35,
+                            )),
+                floatingActionButtonLocation: MediaQuery.of(context)
+                            .viewInsets
+                            .bottom ==
+                        0
+                    ? FloatingActionButtonLocation.centerFloat
+                    : FloatingActionButtonLocation
+                        .endFloat) /*Column(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () => _startDialog(
+                            allSheepQuestions, allSheepQuestionsContexts),
+                        child: const Text('Spytt ut')),
+                    Text("Ikke-filtrert: ${nonFilteredAnswers.toString()}"),
+                    Text("Filtrert:      ${filteredAnswers.toString()}"),
+                    /*ElevatedButton(
                     onPressed: _listen, child: const Text('Spytt inn')),*/
-              ],
-            )));
+                  ],
+                ))*/
+            ));
   }
 }
--- app/lib/utils/question_sets.dart
+++ app/lib/utils/question_sets.dart
@@ -6,9 +6,19 @@ final List<String> allSheepQuestions = [
   'Hvor mange hvite?',
   'Hvor mange svarte?',
   'Hvor mange med svart hode?',
+  'Hvor mange røde slips?',
+  'Hvor mange blå slips?',
+  'Hvor mange gule slips?',
+  'Hvor mange røde øremerker?',
+  'Hvor mange blå øremerker?'
 ];
 
 final List<QuestionContext> allSheepQuestionsContexts = [
+  QuestionContext.numbers,
+  QuestionContext.numbers,
+  QuestionContext.numbers,
+  QuestionContext.numbers,
+  QuestionContext.numbers,
   QuestionContext.numbers,
   QuestionContext.numbers,
   QuestionContext.numbers,
