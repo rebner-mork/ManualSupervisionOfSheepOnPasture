0bfd3983ef84192e0ea2aa9486e41851e94cdf87
Anders H. Rebner <anderhre@stud.ntnu.no>
2022-02-16 12:42:45 +0100

    Moved logic into functions

--- web/lib/my_page/define_ties.dart
+++ web/lib/my_page/define_ties.dart
@@ -100,102 +100,7 @@ class _MyTiesState extends State<MyTies> {
                         )),
                         const DataColumn(label: Text('')),
                       ],
-                      rows: _tieColors
-                              .asMap()
-                              .entries
-                              .map((MapEntry<int, Color> data) =>
-                                  DataRow(cells: [
-                                    DataCell(Container(
-                                        color: !_tiesAdded &&
-                                                !_tiesDeleted &&
-                                                _tieColors[data.key] !=
-                                                    _oldTieColors[data.key]
-                                            ? Colors.orange.shade100
-                                            : null,
-                                        constraints:
-                                            const BoxConstraints(minWidth: 115),
-                                        child: Row(children: [
-                                          DropdownButton<DropdownIcon>(
-                                              value: DropdownIcon(data.value),
-                                              items: possibleColors
-                                                  .map(
-                                                      (Color color) =>
-                                                          DropdownMenuItem<
-                                                                  DropdownIcon>(
-                                                              value:
-                                                                  DropdownIcon(
-                                                                      color),
-                                                              child:
-                                                                  DropdownIcon(
-                                                                          color)
-                                                                      .icon))
-                                                  .toList(),
-                                              onChanged:
-                                                  (DropdownIcon? newIcon) {
-                                                _onColorChanged(
-                                                    newIcon!.icon.color!,
-                                                    data.key,
-                                                    data.value);
-                                              }),
-                                          const SizedBox(width: 8),
-                                          Text(
-                                            colorValueToString[data.value.value]
-                                                .toString(),
-                                            style: largerTextStyle,
-                                          ),
-                                        ]))),
-                                    DataCell(Container(
-                                        color: !_tiesAdded &&
-                                                !_tiesDeleted &&
-                                                _tieLambs[data.key] !=
-                                                    _oldTieLambs[data.key]
-                                            ? Colors.orange.shade100
-                                            : null,
-                                        child: Center(
-                                            child: DropdownButton<int>(
-                                          value: _tieLambs[data.key],
-                                          items: <int>[0, 1, 2, 3, 4, 5, 6]
-                                              .map((int value) =>
-                                                  DropdownMenuItem<int>(
-                                                      value: value,
-                                                      child: Text(
-                                                        value.toString(),
-                                                        style: largerTextStyle,
-                                                      )))
-                                              .toList(),
-                                          onChanged: (int? newValue) {
-                                            _onLambsChanged(newValue, data.key);
-                                          },
-                                        )))),
-                                    DataCell(IconButton(
-                                      icon: Icon(Icons.delete,
-                                          color: Colors.grey.shade800,
-                                          size: 26),
-                                      splashRadius: 22,
-                                      hoverColor: Colors.red,
-                                      onPressed: () {
-                                        showDialog(
-                                            context: context,
-                                            builder: (_) => _deleteTieDialog(
-                                                context, data.key));
-                                      },
-                                    ))
-                                  ]))
-                              .toList() +
-                          [
-                            DataRow(cells: [
-                              DataCell.empty,
-                              DataCell.empty,
-                              DataCell(FloatingActionButton(
-                                mini: true,
-                                child: const Icon(
-                                  Icons.add,
-                                  size: 26,
-                                ),
-                                onPressed: _onTieAdded,
-                              ))
-                            ])
-                          ],
+                      rows: _tieRows() + _newTieRow(),
                     ),
               const SizedBox(height: 10),
               Text(
@@ -206,59 +111,184 @@ class _MyTiesState extends State<MyTies> {
                 textAlign: TextAlign.center,
               ),
               const SizedBox(height: 10),
-              if (_valuesChanged)
-                Column(children: [
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    ElevatedButton(
-                        style: ButtonStyle(
-                            fixedSize: MaterialStateProperty.all(
-                                const Size.fromHeight(35)),
-                            backgroundColor: MaterialStateProperty.all(
-                                _equalValues ? Colors.grey : Colors.green)),
-                        child: Text(
-                          "Lagre",
-                          style: largerTextStyle,
-                          textAlign: TextAlign.center,
-                        ),
-                        onPressed: () => {
-                              if (!_equalValues)
-                                {
-                                  _oldTieColors = _tieColors,
-                                  _oldTieLambs = _tieLambs,
-                                  setState(() {
-                                    _valuesChanged = false;
-                                    _helpText = 'Data er lagret';
-                                    _tiesDeleted = false;
-                                    _tiesAdded = false;
-                                  }),
-                                  _saveTieData()
-                                }
-                            }),
-                    const SizedBox(width: 10),
-                    ElevatedButton(
-                      style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all(
-                              const Size.fromHeight(35)),
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.red)),
-                      child: Text(
-                        "Avbryt",
-                        style: largerTextStyle,
-                      ),
-                      onPressed: () => {
-                        setState(() {
-                          _valuesChanged = false;
-                          _tieColors = List.from(_oldTieColors);
-                          _tieLambs = List.from(_oldTieLambs);
-                          _helpText = '';
-                          _tiesAdded = false;
-                          _tiesDeleted = false;
-                        })
-                      },
-                    ),
-                  ])
-                ])
+              if (_valuesChanged) _saveOrDelete(),
             ])));
+  }
+
+  DataCell _tieCell(int index, Color color) {
+    return DataCell(Container(
+        color: !_tiesAdded &&
+                !_tiesDeleted &&
+                _tieColors[index] != _oldTieColors[index]
+            ? Colors.orange.shade100
+            : null,
+        constraints: const BoxConstraints(minWidth: 115),
+        child: Row(children: [
+          DropdownButton<DropdownIcon>(
+              value: DropdownIcon(color),
+              items: possibleColors
+                  .map((Color color) => DropdownMenuItem<DropdownIcon>(
+                      value: DropdownIcon(color),
+                      child: DropdownIcon(color).icon))
+                  .toList(),
+              onChanged: (DropdownIcon? newIcon) {
+                _onColorChanged(newIcon!.icon.color!, index, color);
+              }),
+          const SizedBox(width: 8),
+          Text(
+            colorValueToString[color.value].toString(),
+            style: largerTextStyle,
+          ),
+        ])));
+  }
+
+  DataCell _lambCell(int index) {
+    return DataCell(Container(
+        color: !_tiesAdded &&
+                !_tiesDeleted &&
+                _tieLambs[index] != _oldTieLambs[index]
+            ? Colors.orange.shade100
+            : null,
+        child: Center(
+            child: DropdownButton<int>(
+          value: _tieLambs[index],
+          items: <int>[0, 1, 2, 3, 4, 5, 6]
+              .map((int value) => DropdownMenuItem<int>(
+                  value: value,
+                  child: Text(
+                    value.toString(),
+                    style: largerTextStyle,
+                  )))
+              .toList(),
+          onChanged: (int? newValue) {
+            _onLambsChanged(newValue, index);
+          },
+        ))));
+  }
+
+  List<DataRow> _tieRows() {
+    return _tieColors
+        .asMap()
+        .entries
+        .map((MapEntry<int, Color> data) => DataRow(cells: [
+              _tieCell(data.key, data.value),
+              _lambCell(data.key),
+              _deleteCell(data.key)
+            ]))
+        .toList();
+  }
+
+  List<DataRow> _newTieRow() {
+    return [
+      DataRow(cells: [
+        DataCell.empty,
+        DataCell.empty,
+        DataCell(FloatingActionButton(
+          mini: true,
+          child: const Icon(
+            Icons.add,
+            size: 26,
+          ),
+          onPressed: _onTieAdded,
+        ))
+      ])
+    ];
+  }
+
+  DataCell _deleteCell(int index) {
+    return DataCell(IconButton(
+      icon: Icon(Icons.delete, color: Colors.grey.shade800, size: 26),
+      splashRadius: 22,
+      hoverColor: Colors.red,
+      onPressed: () {
+        showDialog(
+            context: context, builder: (_) => _deleteTieDialog(context, index));
+      },
+    ));
+  }
+
+  Column _saveOrDelete() {
+    return Column(children: [
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        ElevatedButton(
+            style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all(const Size.fromHeight(35)),
+                backgroundColor: MaterialStateProperty.all(
+                    _equalValues ? Colors.grey : Colors.green)),
+            child: Text(
+              "Lagre",
+              style: largerTextStyle,
+              textAlign: TextAlign.center,
+            ),
+            onPressed: () => {
+                  if (!_equalValues)
+                    {
+                      _oldTieColors = _tieColors,
+                      _oldTieLambs = _tieLambs,
+                      setState(() {
+                        _valuesChanged = false;
+                        _helpText = 'Data er lagret';
+                        _tiesDeleted = false;
+                        _tiesAdded = false;
+                      }),
+                      _saveTieData()
+                    }
+                }),
+        const SizedBox(width: 10),
+        ElevatedButton(
+          style: ButtonStyle(
+              fixedSize: MaterialStateProperty.all(const Size.fromHeight(35)),
+              backgroundColor: MaterialStateProperty.all(Colors.red)),
+          child: Text(
+            "Avbryt",
+            style: largerTextStyle,
+          ),
+          onPressed: () => {
+            setState(() {
+              _valuesChanged = false;
+              _tieColors = List.from(_oldTieColors);
+              _tieLambs = List.from(_oldTieLambs);
+              _helpText = '';
+              _tiesAdded = false;
+              _tiesDeleted = false;
+            })
+          },
+        ),
+      ])
+    ]);
+  }
+
+  BackdropFilter _deleteTieDialog(BuildContext context, int index) {
+    return BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+        child: AlertDialog(
+          title:
+              Text('Slette ${dialogColorToString[_tieColors[index]]} slips?'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop('dialog');
+                  setState(() {
+                    _tieColors.removeAt(index);
+                    _tieLambs.removeAt(index);
+
+                    _valuesChanged = true;
+                    _tiesDeleted = true;
+
+                    _checkEqualColors();
+                    if (_helpText != '') {
+                      _checkEqualLambAmount();
+                    }
+                  });
+                },
+                child: const Text('Ja, slett')),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop('dialog');
+                },
+                child: const Text('Nei, ikke slett'))
+          ],
+        ));
   }
 
   void _onColorChanged(Color newColor, int index, Color ownColor) {
@@ -314,7 +344,6 @@ class _MyTiesState extends State<MyTies> {
     setState(() {
       _tieColors.add(possibleColors.last);
       _tieLambs.add(0);
-      // old.add
       _valuesChanged = true;
       _tiesAdded = true;
 
@@ -323,39 +352,6 @@ class _MyTiesState extends State<MyTies> {
         _checkEqualLambAmount();
       }
     });
-  }
-
-  BackdropFilter _deleteTieDialog(BuildContext context, int index) {
-    return BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-        child: AlertDialog(
-          title:
-              Text('Slette ${dialogColorToString[_tieColors[index]]} slips?'),
-          actions: [
-            TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop('dialog');
-                  setState(() {
-                    _tieColors.removeAt(index);
-                    _tieLambs.removeAt(index);
-
-                    _valuesChanged = true;
-                    _tiesDeleted = true;
-
-                    _checkEqualColors();
-                    if (_helpText != '') {
-                      _checkEqualLambAmount();
-                    }
-                  });
-                },
-                child: const Text('Ja, slett')),
-            TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop('dialog');
-                },
-                child: const Text('Nei, ikke slett'))
-          ],
-        ));
   }
 
   void _readTieData() async {
