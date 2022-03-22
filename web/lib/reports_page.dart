e5c821012704009ccc940947fc7a5a89a4656bdc
Anders H. Rebner <anderhre@stud.ntnu.no>
2022-03-22 13:35:56 +0100

    Replaced web-specific import with package

--- web/lib/reports_page.dart
+++ web/lib/reports_page.dart
@@ -8,8 +8,7 @@ import 'package:pdf/pdf.dart';
 import 'package:web/utils/custom_widgets.dart';
 import 'package:web/utils/styles.dart';
 import 'package:pdf/widgets.dart' as pw;
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
+import 'package:universal_html/html.dart' as html;
 
 final pw.TextStyle columnHeaderTextStyle =
     pw.TextStyle(fontSize: 12, fontWeight: pw.FontWeight.bold);
