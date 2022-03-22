79066c9d1ccf884c840b1acce8d0e0f04b8b0134
Anders H. Rebner <anderhre@stud.ntnu.no>
2022-03-22 08:39:22 +0100

    Dynamic table -and columnwidth on metapage, multipage for triptable

--- web/lib/reports_page.dart
+++ web/lib/reports_page.dart
@@ -11,6 +11,9 @@ import 'package:web/utils/styles.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'dart:html' as html;
 
+final pw.TextStyle columnHeaderTextStyle =
+    pw.TextStyle(fontSize: 12, fontWeight: pw.FontWeight.bold);
+
 class ReportsPage extends StatefulWidget {
   const ReportsPage({Key? key}) : super(key: key);
 
@@ -79,24 +82,32 @@ class _ReportsPageState extends State<ReportsPage> {
 
     Set<String> personnel = {};
 
-    List<QueryDocumentSnapshot<Object?>?> tripsFromYear =
-        []; // TODO: order by date (and possibly startTime)
+// TODO: order by date (and possibly startTime)
+    List<QueryDocumentSnapshot<Object?>?> tripsFromYear = [];
+
     _allTripDocuments.map((QueryDocumentSnapshot tripDoc) {
       if ((tripDoc['startTime'] as Timestamp).toDate().year == _selectedYear) {
         personnel.add(tripDoc[
             'personnelEmail']); // TODO: Change to full name when available
         tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
+        tripsFromYear.add(tripDoc);
       }
     }).toList();
-
-    /*for (DocumentSnapshot tripDoc in _allTripDocuments) {
-      if ((tripDoc['startTime'] as Timestamp).toDate().year == _selectedYear) {
-        //_tripsFromYear.add()
-        //tripAmount++;
-        personnel.add(tripDoc[
-            'personnelEmail']); // TODO: Change to full name when available
-      }
-    }*/
 
     final pw.Document pdf = pw.Document();
 
@@ -112,26 +123,26 @@ class _ReportsPageState extends State<ReportsPage> {
 
   pw.Page metaPdfPage(logoImage, farmDoc, farmOwnerDoc, personnel, tripAmount) {
     return pw.Page(build: (pw.Context context) {
-      return pw.Center(
-          child: pw.Column(children: [
+      return pw.Column(children: [
         pw.Padding(
             padding: const pw.EdgeInsets.only(bottom: 20),
-            child: pw.Text('Årsrapport $_selectedYear',
-                style: const pw.TextStyle(fontSize: 30),
-                textAlign: pw.TextAlign.center)),
-        pw.Image(logoImage, width: 80),
+            child: pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                children: [
+                  pw.Image(logoImage, width: 60),
+                  pw.Text('Årsrapport $_selectedYear',
+                      style: const pw.TextStyle(fontSize: 30),
+                      textAlign: pw.TextAlign.center),
+                  pw.SizedBox(width: 60)
+                ])),
         pw.Table(
-            border: pw.TableBorder.symmetric(
-                inside: const pw.BorderSide(width: 0.5)),
-            columnWidths: const {
-              0: pw.FixedColumnWidth(35),
-              1: pw.FixedColumnWidth(100)
-            },
+            tableWidth: pw.TableWidth.min,
+            border: pw.TableBorder.all(width: 0.5),
             children: [
               pw.TableRow(children: [
                 pw.Padding(
                     padding: const pw.EdgeInsets.all(8),
-                    child: pw.Text('Gård')),
+                    child: pw.Text('Gård', style: columnHeaderTextStyle)),
                 pw.Padding(
                     padding: const pw.EdgeInsets.all(8),
                     child: pw.Text(farmDoc['name'])),
@@ -139,7 +150,7 @@ class _ReportsPageState extends State<ReportsPage> {
               pw.TableRow(children: [
                 pw.Padding(
                     padding: const pw.EdgeInsets.all(8),
-                    child: pw.Text('Adresse')),
+                    child: pw.Text('Adresse', style: columnHeaderTextStyle)),
                 pw.Padding(
                     padding: const pw.EdgeInsets.all(8),
                     child: pw.Text(farmDoc['address'])),
@@ -147,7 +158,7 @@ class _ReportsPageState extends State<ReportsPage> {
               pw.TableRow(children: [
                 pw.Padding(
                     padding: const pw.EdgeInsets.all(8),
-                    child: pw.Text('Gårdseier')),
+                    child: pw.Text('Gårdseier', style: columnHeaderTextStyle)),
                 pw.Padding(
                     padding: const pw.EdgeInsets.all(8),
                     child: pw.Text('E-post: ' +
@@ -158,7 +169,8 @@ class _ReportsPageState extends State<ReportsPage> {
               pw.TableRow(children: [
                 pw.Padding(
                     padding: const pw.EdgeInsets.all(8),
-                    child: pw.Text('Oppsynspersonell')),
+                    child: pw.Text('Oppsynspersonell',
+                        style: columnHeaderTextStyle)),
                 pw.Padding(
                     padding: const pw.EdgeInsets.all(8),
                     child: pw.Text(personnel
@@ -170,123 +182,120 @@ class _ReportsPageState extends State<ReportsPage> {
               pw.TableRow(children: [
                 pw.Padding(
                     padding: const pw.EdgeInsets.all(8),
-                    child: pw.Text('Antall oppsynsturer')),
+                    child: pw.Text('Antall oppsynsturer',
+                        style: columnHeaderTextStyle)),
                 pw.Padding(
                     padding: const pw.EdgeInsets.all(8),
                     child: pw.Text('$tripAmount')),
               ]),
             ])
-      ]));
+      ]);
     });
   }
 
-  pw.Page pdfTripsTable(List<QueryDocumentSnapshot<Object?>?> trips) {
-    final pw.TextStyle columnHeaderTextStyle =
-        pw.TextStyle(fontSize: 13, fontWeight: pw.FontWeight.bold);
-
-    return pw.Page(
+  pw.MultiPage pdfTripsTable(List<QueryDocumentSnapshot<Object?>?> trips) {
+    return pw.MultiPage(
         pageFormat: PdfPageFormat(
             PdfPageFormat.a4.width, PdfPageFormat.a4.height,
-            marginAll: 1 * PdfPageFormat.cm // 2 default
-            ),
+            marginAll: 1 * PdfPageFormat.cm),
         build: (pw.Context context) {
-          return pw.Center(
-              child: pw.Table(
-                  border: pw.TableBorder.symmetric(
-                      inside: const pw.BorderSide(width: 0.5),
-                      outside: const pw.BorderSide(width: 0.5)),
-                  columnWidths: const {
-                0: pw.FixedColumnWidth(46),
-                1: pw.FixedColumnWidth(55),
-                2: pw.FixedColumnWidth(35),
-                3: pw.FixedColumnWidth(40),
-                4: pw.FixedColumnWidth(35),
-                5: pw.FixedColumnWidth(38),
-                6: pw.FixedColumnWidth(35),
-                7: pw.FixedColumnWidth(40),
-              },
-                  children: [
-                pw.TableRow(children: [
-                  pw.Padding(
-                      padding: const pw.EdgeInsets.all(8),
-                      child: pw.Text('Dato',
-                          style: columnHeaderTextStyle,
-                          textAlign: pw.TextAlign.center)),
-                  pw.Padding(
-                      padding: const pw.EdgeInsets.all(8),
-                      child: pw.Text('Tidsperiode',
-                          style: columnHeaderTextStyle,
-                          textAlign: pw.TextAlign.center)),
-                  pw.Padding(
-                      padding: const pw.EdgeInsets.all(8),
-                      child: pw.Text('Sauer\ntotalt',
-                          style: columnHeaderTextStyle,
-                          textAlign: pw.TextAlign.center)),
-                  pw.Padding(
-                      padding: const pw.EdgeInsets.all(8),
-                      child: pw.Text('Voksne',
-                          style: columnHeaderTextStyle,
-                          textAlign: pw.TextAlign.center)),
-                  pw.Padding(
-                      padding: const pw.EdgeInsets.all(8),
-                      child: pw.Text('Lam',
-                          style: columnHeaderTextStyle,
-                          textAlign: pw.TextAlign.center)),
-                  pw.Padding(
-                      padding: const pw.EdgeInsets.all(8),
-                      child: pw.Text('Skadde',
-                          style: columnHeaderTextStyle,
-                          textAlign: pw.TextAlign.center)),
-                  pw.Padding(
-                      padding: const pw.EdgeInsets.all(8),
-                      child: pw.Text('Døde',
-                          style: columnHeaderTextStyle,
-                          textAlign: pw.TextAlign.center)),
-                  pw.Padding(
-                      padding: const pw.EdgeInsets.all(8),
-                      child: pw.Text('Rovdyr',
-                          style: columnHeaderTextStyle,
-                          textAlign: pw.TextAlign.center)),
-                ]),
-                ...trips.map((QueryDocumentSnapshot<Object?>? tripDoc) {
-                  DateTime startTime =
-                      (tripDoc!['startTime']! as Timestamp).toDate();
-                  DateTime stopTime =
-                      (tripDoc['stopTime']! as Timestamp).toDate();
+          return [
+            pw.Table(
+                border: pw.TableBorder.symmetric(
+                    inside: const pw.BorderSide(width: 0.5),
+                    outside: const pw.BorderSide(width: 0.5)),
+                columnWidths: const {
+                  0: pw.FixedColumnWidth(50),
+                  1: pw.FixedColumnWidth(50),
+                  2: pw.FixedColumnWidth(35),
+                  3: pw.FixedColumnWidth(35),
+                  4: pw.FixedColumnWidth(35),
+                  5: pw.FixedColumnWidth(35),
+                  6: pw.FixedColumnWidth(35),
+                  7: pw.FixedColumnWidth(35),
+                },
+                children: [
+                  pw.TableRow(children: [
+                    pw.Padding(
+                        padding: const pw.EdgeInsets.all(8),
+                        child: pw.Text('Dato',
+                            style: columnHeaderTextStyle,
+                            textAlign: pw.TextAlign.center)),
+                    pw.Padding(
+                        padding: const pw.EdgeInsets.all(8),
+                        child: pw.Text('Tid',
+                            style: columnHeaderTextStyle,
+                            textAlign: pw.TextAlign.center)),
+                    pw.Padding(
+                        padding: const pw.EdgeInsets.all(8),
+                        child: pw.Text('Sauer\ntotalt',
+                            style: columnHeaderTextStyle,
+                            textAlign: pw.TextAlign.center)),
+                    pw.Padding(
+                        padding: const pw.EdgeInsets.all(8),
+                        child: pw.Text('Voksne',
+                            style: columnHeaderTextStyle,
+                            textAlign: pw.TextAlign.center)),
+                    pw.Padding(
+                        padding: const pw.EdgeInsets.all(8),
+                        child: pw.Text('Lam',
+                            style: columnHeaderTextStyle,
+                            textAlign: pw.TextAlign.center)),
+                    pw.Padding(
+                        padding: const pw.EdgeInsets.all(8),
+                        child: pw.Text('Skadde',
+                            style: columnHeaderTextStyle,
+                            textAlign: pw.TextAlign.center)),
+                    pw.Padding(
+                        padding: const pw.EdgeInsets.all(8),
+                        child: pw.Text('Døde',
+                            style: columnHeaderTextStyle,
+                            textAlign: pw.TextAlign.center)),
+                    pw.Padding(
+                        padding: const pw.EdgeInsets.all(8),
+                        child: pw.Text('Rovdyr',
+                            style: columnHeaderTextStyle,
+                            textAlign: pw.TextAlign.center)),
+                  ]),
+                  ...trips.map((QueryDocumentSnapshot<Object?>? tripDoc) {
+                    DateTime startTime =
+                        (tripDoc!['startTime']! as Timestamp).toDate();
+                    DateTime stopTime =
+                        (tripDoc['stopTime']! as Timestamp).toDate();
 
-                  return pw.TableRow(children: [
-                    pw.Padding(
-                        padding: const pw.EdgeInsets.all(8),
-                        child: pw.Text(
-                            '${startTime.day.toString().padLeft(2, '0')}/${startTime.month.toString().padLeft(2, '0')}/${startTime.year}',
-                            textAlign: pw.TextAlign.center)),
-                    pw.Padding(
-                        padding: const pw.EdgeInsets.all(8),
-                        child: pw.Text(
-                            // Klokkeslett rett fra db er feil? Blir riktig etter å ha endret dem i Firestore(?)
-                            '${startTime.hour}:${startTime.minute.toString().padLeft(2, '0')}-${stopTime.hour}:${stopTime.minute.toString().padLeft(2, '0')}',
-                            textAlign: pw.TextAlign.center)),
-                    pw.Padding(
-                        padding: const pw.EdgeInsets.all(8),
-                        child: pw.Text('0', textAlign: pw.TextAlign.center)),
-                    pw.Padding(
-                        padding: const pw.EdgeInsets.all(8),
-                        child: pw.Text('1', textAlign: pw.TextAlign.center)),
-                    pw.Padding(
-                        padding: const pw.EdgeInsets.all(8),
-                        child: pw.Text('2', textAlign: pw.TextAlign.center)),
-                    pw.Padding(
-                        padding: const pw.EdgeInsets.all(8),
-                        child: pw.Text('3', textAlign: pw.TextAlign.center)),
-                    pw.Padding(
-                        padding: const pw.EdgeInsets.all(8),
-                        child: pw.Text('4', textAlign: pw.TextAlign.center)),
-                    pw.Padding(
-                        padding: const pw.EdgeInsets.all(8),
-                        child: pw.Text('5', textAlign: pw.TextAlign.center)),
-                  ]);
-                }).toList()
-              ]));
+                    return pw.TableRow(children: [
+                      pw.Padding(
+                          padding: const pw.EdgeInsets.all(8),
+                          child: pw.Text(
+                              '${startTime.day.toString().padLeft(2, '0')}/${startTime.month.toString().padLeft(2, '0')}/${startTime.year}',
+                              textAlign: pw.TextAlign.center)),
+                      pw.Padding(
+                          padding: const pw.EdgeInsets.all(8),
+                          child: pw.Text(
+                              '${startTime.hour}:${startTime.minute.toString().padLeft(2, '0')} - ${stopTime.hour}:${stopTime.minute.toString().padLeft(2, '0')}',
+                              textAlign: pw.TextAlign.center)),
+                      pw.Padding(
+                          padding: const pw.EdgeInsets.all(8),
+                          child: pw.Text('0', textAlign: pw.TextAlign.center)),
+                      pw.Padding(
+                          padding: const pw.EdgeInsets.all(8),
+                          child: pw.Text('1', textAlign: pw.TextAlign.center)),
+                      pw.Padding(
+                          padding: const pw.EdgeInsets.all(8),
+                          child: pw.Text('2', textAlign: pw.TextAlign.center)),
+                      pw.Padding(
+                          padding: const pw.EdgeInsets.all(8),
+                          child: pw.Text('3', textAlign: pw.TextAlign.center)),
+                      pw.Padding(
+                          padding: const pw.EdgeInsets.all(8),
+                          child: pw.Text('4', textAlign: pw.TextAlign.center)),
+                      pw.Padding(
+                          padding: const pw.EdgeInsets.all(8),
+                          child: pw.Text('5', textAlign: pw.TextAlign.center)),
+                    ]);
+                  }).toList()
+                ])
+          ];
         });
   }
 
@@ -332,11 +341,11 @@ class _ReportsPageState extends State<ReportsPage> {
                   ElevatedButton(
                       onPressed: _downloadReport,
                       child: const Text(
-                        "Generer rapport",
-                        style: TextStyle(fontSize: 20),
+                        "Last ned årsrapport",
+                        style: TextStyle(fontSize: 22),
                       ),
                       style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(180, 50),
+                        fixedSize: const Size(230, 60),
                       ))
                 ],
               )
