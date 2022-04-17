31e05a47e513942ee15fb7485ae2e9d5a82a1a24
morkolai <nikolai-mork@live.no>
2022-04-17 14:11:59 +0200

    merge dropdown item and dropdown widget files

--- app/lib/register/widgets/tie_dropdown.dart
+++ app/lib/register/widgets/tie_dropdown.dart
@@ -1,6 +1,7 @@
-import 'package:app/register/widgets/tie_dropdown_item.dart';
 import 'package:app/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:app/utils/constants.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
 
 class TieDropdownButton extends StatelessWidget {
   const TieDropdownButton(
@@ -38,5 +39,41 @@ class TieDropdownButton extends StatelessWidget {
                       ))
               .toList(),
         ));
+  }
+}
+
+class TieDropDownItem extends StatelessWidget {
+  TieDropDownItem({Key? key, required String colorHex}) : super(key: key) {
+    bool isTransparent = colorHex == '0';
+    icon = Icon(
+        isTransparent ? Icons.disabled_by_default : FontAwesome5.black_tie,
+        color:
+            isTransparent ? Colors.grey.shade400 : colorStringToColor[colorHex],
+        size: 38);
+
+    label = colorValueToStringGui[colorHex]!;
+  }
+
+  late final Icon icon;
+  late final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        icon,
+        const SizedBox(
+          width: 10,
+        ),
+        SizedBox(
+            width: 90,
+            child: Text(
+              label,
+              style: dropDownTextStyle,
+            ))
+      ],
+    ));
   }
 }
--- web/lib/my_page/define_ties/tie_or_eartag_dropdown.dart
+++ web/lib/my_page/define_ties/tie_or_eartag_dropdown.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:web/my_page/define_ties/tie_or_eartag_dropdown_item.dart';
 import 'package:web/utils/styles.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:web/utils/constants.dart';
 
 class TieOrEartagDropdownButton extends StatelessWidget {
   const TieOrEartagDropdownButton(
@@ -34,6 +35,47 @@ class TieOrEartagDropdownButton extends StatelessWidget {
                     child: TieDropDownItem(color: color, isTie: isTie),
                   ))
               .toList(),
+        ));
+  }
+}
+
+class TieDropDownItem extends StatelessWidget {
+  TieDropDownItem({Key? key, required Color color, required bool isTie})
+      : super(key: key) {
+    bool isTransparent = color == Colors.transparent;
+    icon = Icon(
+        isTransparent
+            ? Icons.disabled_by_default
+            : isTie
+                ? FontAwesome5.black_tie
+                : Icons.local_offer,
+        color: isTransparent ? Colors.grey.shade400 : color,
+        size: 30);
+
+    label = colorValueToStringGui[color.value.toRadixString(16)]!;
+  }
+
+  late final Icon icon;
+  late final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.transparent,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon,
+            const SizedBox(
+              width: 10,
+            ),
+            SizedBox(
+                width: 55,
+                child: Text(
+                  label,
+                  style: dropDownTextStyle,
+                ))
+          ],
         ));
   }
 }
